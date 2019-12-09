f6c4e96aa26bea47ee0784d49b252a47fad57fd4
Sanjay <panda.sanjay18@gmail.com>
2019-12-09 23:40:12 +0000

    Document update and package json

--- scripts/copyPackage.js
+++ scripts/copyPackage.js
@@ -3,4 +3,5 @@ import fs from "fs";
 import pkg from "../package.json";
 pkg.name = argv.name;
 delete pkg.prepare;
-const writable = fs.writeFileSync("./lib/package.json", JSON.stringify(pkg));
+delete pkg.devDependencies;
+fs.writeFileSync("./lib/package.json", JSON.stringify(pkg));
